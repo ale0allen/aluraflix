4832a8fd8565026c2b09700ab9949cb02210080b
Alexandre Abreu Ferreira <alexandre@elaw.com.br>
2022-11-09 10:55:24 -0300

    aula 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -10,8 +10,9 @@ import CadastroCategoria from './pages/cadastro/Categoria';
 // Desafio master blaster na descrição
 // Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
 const Pagina404 = () => (<div>Página 404</div>)
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
@@ -19,6 +20,5 @@ ReactDOM.render(
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
